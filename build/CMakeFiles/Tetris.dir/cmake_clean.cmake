2a79957d60a447c776eeaa5634cfd85955e75379
Mihails 'Delinx' Mozajevs <mihails@teamilluminate.dev>
2022-11-20 15:54:47 +0000

    init

---
+++ build/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/main.cpp.o"
+  "CMakeFiles/Tetris.dir/main.cpp.o.d"
+  "Tetris"
+  "Tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ plan.md
@@ -0,0 +1,5 @@
+* Seperate actions from display, needed for animation
+* make main loop which can be progressed by time, action, event
+* add basic tetris functionality
+* add special abilities
+
